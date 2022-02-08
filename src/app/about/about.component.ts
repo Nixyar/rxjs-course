c456ba95d520d65a7134002a9c4190d03f273ca4
kozlov-oe <O.Kozlov@otpbank.ru>
2022-02-08 23:50:36 +0300

    added filter for courses

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,8 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {noop, Observable} from 'rxjs';
-import {response} from 'express';
-import {createNewObservable} from '../common/util';
-import {map} from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'about',
@@ -14,15 +10,7 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const http$ = createNewObservable('/api/courses');
 
-    const courses$ = http$.pipe(
-      map(res  => Object.values(res['payload']))
-    );
-
-    courses$.subscribe((courses: any) => {
-      console.log(courses);
-    }, noop, () => console.log('complete'));
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,34 @@
 import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
+import {Course} from '../model/course';
+import {interval, noop, Observable, of, timer} from 'rxjs';
 import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import {createNewObservable} from '../common/util';
 
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  beginnerCourses: Course[];
+  advancedCourses: Course[];
 
+  constructor() {
 
-    constructor() {
+  }
 
-    }
+  ngOnInit() {
+    const http$ = createNewObservable('/api/courses');
 
-    ngOnInit() {
+    const courses$ = http$.pipe(
+      map(res => Object.values(res['payload']))
+    );
 
-
-
-    }
+    courses$.subscribe((courses: any) => {
+      this.beginnerCourses = courses.filter(item => item.category === 'BEGINNER');
+      this.advancedCourses = courses.filter(item => item.category === 'ADVANCED');
+    }, noop, () => console.log('complete'));
+  }
 
 }
